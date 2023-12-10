e5fa7ff7da4f6d8ba878f431104c8152562f2be4
Pranav0305 <119056415+Pranav0305@users.noreply.github.com>
2023-12-10 05:35:00 +0530

    Delete Mid_Submission_2022363_2022310 directory

--- Mid_Submission_2022363_2022310/src/main/java/com/example/mid_submission_2022363_2022310/Menu.java
+++
@@ -1,7 +0,0 @@
-package com.example.mid_submission_2022363_2022310;
-
-public interface Menu
-{
-    public void resume();
-    public void exit();
-}
--- Mid_Submission_2022363_2022310/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.mid_submission_2022363_2022310 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.mid_submission_2022363_2022310 to javafx.fxml;
-    exports com.example.mid_submission_2022363_2022310;
-}
